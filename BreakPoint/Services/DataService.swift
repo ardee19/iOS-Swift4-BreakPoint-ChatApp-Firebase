917cbf52171b34361227e17cd714d8dae2d076df
Roy Dimayuga <mr_nicelittleboy@yahoo.com>
2017-08-07 17:36:57 -0700

    Setup Login UI

---
+++ BreakPoint/Services/DataService.swift
@@ -0,0 +1,26 @@
+//
+//  DataService.swift
+//  BreakPoint
+//
+//  Created by Super Awesome on 8/7/17.
+//  Copyright © 2017 Roy Dimayuga. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+let DB_BASE = Database.database().reference()
+
+class DataService {
+    static let instance = DataService()
+    
+    public private(set) var REF_BASE = DB_BASE
+    public private(set) var REF_USERS = DB_BASE.child("users")
+    public private(set) var REF_GROUPS = DB_BASE.child("groups")
+    public private(set) var REF_FEED = DB_BASE.child("feed")
+    
+    func createDBUser(uid: String, userData: Dictionary<String,Any>) {
+        REF_USERS.child(uid).updateChildValues(userData)
+    }
+    
+}
