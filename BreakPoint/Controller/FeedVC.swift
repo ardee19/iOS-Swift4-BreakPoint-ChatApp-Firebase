62c74b88485783adf57b1f244a30377b3db801b2
Roy Dimayuga <mr_nicelittleboy@yahoo.com>
2017-08-08 01:20:46 -0700

    Setup FeedVC and GroupsVC UI

--- BreakPoint/Controller/FeedVC.swift
+++ BreakPoint/Controller/FeedVC.swift
@@ -1,5 +1,5 @@
 //
-//  SecondViewController.swift
+//  FeedVC.swift
 //  BreakPoint
 //
 //  Created by Super Awesome on 8/7/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class FeedVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- BreakPoint/Controller/LoginVC.swift
+++ BreakPoint/Controller/LoginVC.swift
@@ -10,26 +10,53 @@ import UIKit
 
 class LoginVC: UIViewController {
 
+    // Outlets
+    @IBOutlet weak var emailField: InsetTextField!
+    @IBOutlet weak var passwordField: InsetTextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        emailField.delegate = self
+        passwordField.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func signInPressed(_ sender: Any) {
+        
+        let email = emailField.text!
+        let password = passwordField.text!
+
+        if email != "" && password != ""  {
+            AuthService.instance.loginUser(withEmail: email, andPassword: password, loginComplete: { (success, loginError) in
+                
+                if success {
+                    print("RD: Login Successful")
+                    self.dismiss(animated: true, completion: nil)
+                } else {
+                    print(String(describing: loginError?.localizedDescription))
+                    AuthService.instance.registerUser(withEmail: email, andPassword: password, userCreationComplete: { (success, error) in
+                        if success {
+                            AuthService.instance.loginUser(withEmail: email, andPassword: password, loginComplete: { (success, _) in
+                                print("RD: Registration Successful")
+                                self.dismiss(animated: true, completion: nil)
+                            })
+                        } else {
+                            print(error?.localizedDescription as Any)
+                        }
+                    })
+                }
+                
+            })
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func closeButtonPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
-    */
+}
 
+extension LoginVC: UITextFieldDelegate {
+    
+    
 }
