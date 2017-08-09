da7d2f3e80d1159a6099b7b020b596d1ceb7f576
Roy Dimayuga <mr_nicelittleboy@yahoo.com>
2017-08-08 17:05:17 -0700

    Setup MeVC

---
+++ BreakPoint/Controller/MeVC.swift
@@ -0,0 +1,30 @@
+//
+//  MeVC.swift
+//  BreakPoint
+//
+//  Created by Super Awesome on 8/8/17.
+//  Copyright © 2017 Roy Dimayuga. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+    
+    // Outlets
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    @IBAction func signOutPressed(_ sender: Any) {
+        
+    }
+    
+}
